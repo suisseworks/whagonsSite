0b05942c828bae9535b33dda2f064b7021c201ed
Matthias Malek <matthiasmalek72@gmail.com>
2023-09-18 07:29:17 -0600

    changes by matthias

---
+++ gamificacion-en-apps-colaborativas.php
@@ -0,0 +1,345 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Gamificación en Apps colaborativas</title>
+
+    <?php include 'inc/header-blog.php'; ?>
+
+   
+
+
+</head>
+
+<body>
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-BCR7QH6"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+    <!-- Start Navbar Area -->
+    <div class="navbar-area pakap-new-navbar-area blog-header">
+        <div class="pakap-responsive-nav">
+            <div class="container">
+                <div class="pakap-responsive-menu">
+                    <div class="logo">
+                        <a href="/"><img src="assets/img/custom/black-logo.png" alt="logo"></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pakap-nav">
+            <div class="container">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <a class="navbar-brand" href="/"><img src="assets/img/custom/black-logo.png" alt="logo"></a>
+                    <div class="collapse navbar-collapse mean-menu">
+                        <ul class="navbar-nav">
+                        <li class="nav-item"><a href="blog.php" class="nav-link  ">Regresar al Blog</a></li>
+                        </ul>
+                        <div class="others-option">
+                            <a href="contactar.php" class="default-btn">Comenzar</a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+
+    <section style="  background: url(assets/img/custom/press2.png);   background-size: cover;
+    background-repeat: no-repeat;" class="blog-details-banner" >
+        <div class="blog-details-banner-overlay">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="section-title">
+                            <h2 class="text-white">Gamificación en Apps Colaborativas</h2> 
+                            <!-- <p class="text-white">
+                                Lorem Ipsum Dolor set amet
+                            </p> -->
+                            <p class="b-date mb-2 text-white">
+                                Marzo, 2023                      
+                            </p>
+                        </div>
+                    </div>    
+                </div>
+            </div>    
+        </div>      
+    </section>
+
+    <section class="lp-area pt-75">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-lg-8 col-md-12">
+                     <div class="row">
+                        <div class="col-lg-12">
+                            <div class="single-blog-wrap-post pe-0 pe-lg-5 pe-md-0">
+                                <div class="">
+                                    <img src="assets/img/custom/press2.png" alt="blog">   
+                                </div>
+                                <div class="content">
+                                    <p class="b-date mb-2">
+                                      Abril, 2023                    
+                                    </p>
+                                   
+                                     <h1>Gamification en apps colaborativas </h1>  
+                                   
+                                    <p>
+                                    <b>Cómo motivar a los usuarios y mejorar la eficacia.</b>
+                                    </p>
+                                    <p>
+                                    <ul class="blog">
+                                        <li>La gamificación es el proceso de <b>agregar elementos de juego a situaciones no relacionadas con los juegos. </b></li>
+                                        <li>En las aplicaciones colaborativas, la gamificación puede ser <b>utilizada para motivar a los usuarios </b> y mejorar la eficacia de la aplicación.</li>
+                                        <li>La gamificación se puede implementar mediante la adición de elementos de juego como recompensas, puntajes y tablas de clasificación.</li>
+                                        <li>Los beneficios de la gamificación en aplicaciones colaborativas incluyen el aumento de la motivación y el compromiso de los usuarios, así como la mejora de la eficacia de la aplicación.</li>
+                                        <li>Al diseñar la gamificación, es importante considerar la cultura de la empresa y el público objetivo.</li>
+                                    </ul> 
+                                    <br>
+                                    </p>
+                                    <p><b>Ejemplos de gamificación en aplicaciones colaborativas:</b></p>
+                                    <p>
+                                    <ul class="blog">
+                                        <li>Agregar <b>recompensas y puntajes</b> en aplicaciones de gestión de proyectos.</li>
+                                        <li>Utilizar <b>tablas de clasificación</b> para fomentar la competencia amistosa entre los usuarios</li>
+                                        <li>Ofrecer <b>desafíos semanales o mensuales </b>para motivar a los usuarios a lograr ciertas metas</li>
+                                        <li>Implementar un <b>sistema de niveles </b>para recompensar a los usuarios por su progreso y logros</li>
+                                    </ul> 
+                                    </p>
+                                    <p>En resumen, la gamificación en las aplicaciones colaborativas puede ser una solución efectiva para motivar a los usuarios y mejorar la eficacia de la aplicación. Al agregar elementos de juego, los usuarios pueden sentir que están compitiendo y trabajando juntos para lograr un objetivo común. Esto puede aumentar la satisfacción del usuario y reducir la tasa de abandono, además de mejorar la eficiencia de la empresa y aumentar la rentabilidad.</p>
+
+                                </div>
+                            </div>
+                        </div> 
+
+                    </div>   
+                </div>
+                <div class="col-lg-4 col-md-12">
+                      <div class="ps-lg-5">
+                            <div class="col-md-12">
+                                <div class="key-features-card style-two news-l post-key-card">
+                                    <h2 class="text-center mb-4">
+                                        Boletín Informativo
+                                    </h2>
+                                    <div class="contact-form">
+                                        <form id="subscribe-form">
+                                            <div class="form-group">
+                                                <input type="email" name="email" class="form-control" id="email" required="" placeholder="Tu correo electrónico">
+                                            </div>
+                                            <div class="col-md-12 text-left">
+                                                <button type="submit" class="default-btn subscribe-btn">Mantenerme informado</button>
+                                            </div>    
+                                        </form>
+                                    </div>    
+                                </div>    
+                            </div>    
+                    
+            
+                                <!-- <div class="py-5">
+                                    <h3 class="sub-title">Categories</h3>
+                                    <p>Guido Tenorio - Gerente de Operaciones del Grupo Lagar.Guido Tenorio - Gerente de Operaciones del Grupo Lagar</p>
+                                </div> -->
+                            
+
+                           <!-- <div class="col-12">
+                               <div class="single-blog-wrap-post d-flex side-bar-pp">
+                                  <div class="image">
+                                     <a target="_blank" href="#" class="d-block">
+                                     </a>
+                                  </div>
+                                  <div class="content ps-4 mt-0">
+                                    <p class="b-date mb-2">
+                                    — LONG READS
+                                    </p>
+                                    <h3><a href="#">Oranges And Lemons For The Fresh Start</a></h3>
+                                  </div>
+                                </div>
+                           </div>
+
+                           <div class="col-12">
+                               <div class="single-blog-wrap-post d-flex side-bar-pp">
+                                  <div class="image">
+                                     <a target="_blank" href="#" class="d-block">
+                                     </a>
+                                  </div>
+                                  <div class="content ps-4 mt-0">
+                                    <p class="b-date mb-2">
+                                    — LONG READS
+                                    </p>
+                                    <h3><a href="#">Oranges And Lemons For The Fresh Start</a></h3>
+                                  </div>
+                                </div>
+                           </div>
+
+                           <div class="col-12">
+                               <div class="single-blog-wrap-post d-flex side-bar-pp">
+                                  <div class="image">
+                                     <a target="_blank" href="#" class="d-block">
+                                     </a>
+                                  </div>
+                                  <div class="content ps-4 mt-0">
+                                    <p class="b-date mb-2">
+                                    — LONG READS
+                                    </p>
+                                    <h3><a href="#">Oranges And Lemons For The Fresh Start</a></h3>
+                                  </div>
+                                </div>
+                           </div>
+
+                           <div class="py-5">
+                                <form class="d-flex">
+                                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                                    <button class="default-btn" type="submit">Search</button>
+                                </form>
+                           </div> -->
+                                
+                           <div class="single-footer-widget side-bar-icons pb-5">
+                             <h3 class="sub-title">SÍGUENOS</h3>
+                                    <ul class="social-links">
+                                        <li><a href="https://www.facebook.com/whagons" target="_blank"><i class="ri-facebook-fill"></i></a></li>
+                                        <li><a href="https://www.instagram.com/whagons/" target="_blank"><i class="ri-instagram-fill"></i></a></li>
+                                        <li><a href="https://www.linkedin.com/company/78597367/" target="_blank"><i class="ri-linkedin-fill"></i></a></li>
+                                    </ul>
+                              </div>
+                           </div>
+                      </div>
+                </div>    
+            </div>
+        </section>
+
+        <!-- <section class="blog-message-sec">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class=" contact-form mb-4 mb-lg-0">
+                            <div class="d-flex justify-content-start my-5">
+                                <h2>Leave A Message</h2>
+                            </div>
+                            <form id="blog-details-form">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input type="text" name="name" class="form-control" placeholder="nombre">
+                                        </div>    
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <input type="email" name="email" class="form-control" placeholder="correo electrónico">
+                                        </div>    
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <textarea name="message" cols="30" rows="6" class="form-control" placeholder="mensaje..."></textarea>
+                                        </div>    
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <button type="submit" class="default-btn">Enviar Mensaje</button>
+                                        </div>    
+                                    </div>    
+                                </div>    
+                            </form>
+                        </div>    
+                    </div>
+                </div>
+            </div>    
+        </section> -->
+
+        <section class="popular_sec pb-75">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="d-flex justify-content-start my-5">
+                            <h2>Posts Populares</h2>
+                        </div>
+                            
+                        <div class="row justify-content-center">
+                            <div class="col-lg-3 col-md-6">
+                                <div class="single-blog-wrap-post ">
+                                    <div class="image">
+                                        <a target="_blank" href="#" class="d-block">
+                                            <img src="assets/img/custom/press2.png" alt="blog">
+                                        </a>
+                                     </div>
+                                    <div class="content">
+                                     <p class="b-date mb-2">
+                                        SEP 9, 2022 — HEALTH
+                                     </p>
+                                       <h3><a href="#">Oranges And Lemons For The Fresh Start</a></h3>
+                                    </div>
+                                </div>
+                            </div>
+            
+                            <div class="col-lg-3 col-md-6">
+                                <div class="single-blog-wrap-post">
+                                    <div class="image">
+                                        <a target="_blank" href="#" class="d-block">
+                                         <img src="assets/img/custom/press2.png" alt="blog">
+                                        </a>
+                                     </div>
+                                    <div class="content">
+                                     <p class="b-date mb-2">
+                                        SEP 9, 2022 — HEALTH
+                                     </p>
+                                       <h3><a href="#">Oranges And Lemons For The Fresh Start</a></h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6">
+                                <div class="single-blog-wrap-post">
+                                    <div class="image">
+                                        <a target="_blank" href="#" class="d-block">
+                                            <img src="assets/img/custom/press1.png" alt="blog">
+                                        </a>
+                                     </div>
+                                    <div class="content">
+                                     <p class="b-date mb-2">
+                                        SEP 9, 2022 — HEALTH
+                                     </p>
+                                       <h3><a href="#">Oranges And Lemons For The Fresh Start</a></h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-3 col-md-6 ">
+                                <div class="single-blog-wrap-post">
+                                    <div class="image">
+                                        <a target="_blank" href="#" class="d-block">
+                                         <img src="assets/img/custom/press2.png" alt="blog">
+                                        </a>
+                                     </div>
+                                    <div class="content">
+                                     <p class="b-date mb-2">
+                                        SEP 9, 2022 — HEALTHs
+                                     </p>
+                                       <h3><a href="#">Oranges And Lemons For The Fresh Start</a></h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>    
+        </section>
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <a href="https://api.whatsapp.com/send?phone=50672286148" class="float" target="_blank">
+            <i class="fa fa-whatsapp my-float"></i>
+        </a>
+
+
+
+    <?php include 'inc/footer.php'; ?>
+    <script>
+        
+        /* $(function() {
+           
+            if (navigator.language.includes('en'))
+                location.href = 'en';
+        });
+
+        */
+    </script>
+</body>
+
+</html>
